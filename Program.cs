7c1fd2b132d1c016e83ac950b5b59a13b1a537cb
Eldorbek94 <eldorbtc@gmail.com>
2024-10-30 16:19:06 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Exchange Rate ");
+Console.WriteLine("Qiymat:");
+string q = Console.ReadLine();
+Console.WriteLine("Valyuta kursi (so'm):");
+string c= Console.ReadLine();
+
+decimal summ = Convert.ToDecimal(q)*Convert.ToDecimal(c);
+
+Console.WriteLine($"Output: {summ} so'm");
